752fc920f54e63bde95abea8b1ec1249fc0e289d
Eddy <you@example.com>
2021-04-03 15:20:42 +0300

    hw

--- src/App.js
+++ src/App.js
@@ -4,31 +4,43 @@ import React, { useState } from "react";
 // якщо у нас масив з 3 елементів і ми клікнули на кнопку 3 рази, то на екрані жодна карточка не має відмалюватись
 // (кнопки повернення до початкового стану не треба
 
+// 1. Змінити,щоб видаляло з кінця
+// 2. Додати кнопку повернення всіх юзерів 
+
+
+const items = [
+  {
+    name: "Ed",
+    age: 20,
+  },
+  {
+    name: "Sanya",
+    age: 19,
+  },
+  {
+    name: "Taras",
+    age: 18,
+  },
+];
+
 function App() {
-  let [counter, setCounter] = useState(0);
-  let [user, setUser] = useState({ name: "vasya", age: 23 });
+  const[users, changeUsers] = useState(items)
 
-  const incCounter = () => {
-    counter++;
-    console.log(counter);
-    setCounter(counter);
-  };
-
-  const incAge = () => {
-    user.age++;
-    console.log(user);
-    setUser({ ...user });
-  };
+const removeUsers = () => {
+  let removed = users.shift();
+  let newArr = users.filter(item => item.name != removed.name)
+  changeUsers(newArr)
+}
 
   return (
     <div>
-      <div>
-        {counter} - <button onClick={incCounter}>increment counter</button>
-      </div>
-      <div>
-        {user.name} - {user.age}
-        <button onClick={incAge}> inc age </button>
-      </div>
+      <h1>Hello Users</h1>
+      <ul>
+        {users.map((item) => {
+          return <li key={item.name}>{item.name} {item.age}</li>;
+        })}
+      </ul>
+      <button onClick={removeUsers}>remove</button>
     </div>
   );
 }
